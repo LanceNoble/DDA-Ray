07795dbaed512cd9c8565a92adf27ce92238953f
LanceNoble <ltnlikespie@gmail.com>
2025-07-14 15:31:09 -0400

    Remove function for recursively casting rays

--- utils.lua
+++ utils.lua
@@ -33,8 +33,8 @@ function utils.castRay(x, y, radians, map)
 
         -- Add yDirection and xDirection at the end so the ray does not undershoot the tile it was supposed to hit
         -- Multiply yDirection and xDirection by 0.1 so the ray does not overshoot the tile it was supposed to hit
-        local i = y + yDirection * rayDistance + yDirection * 0.01
-        local j = x + xDirection * rayDistance + xDirection * 0.01
+        local i = y + yDirection * rayDistance + yDirection * .01
+        local j = x + xDirection * rayDistance + xDirection * .01
 
         -- Stop ray from clipping through diagonal walls
         local k = y + yDirection * rayDistance
@@ -42,21 +42,6 @@ function utils.castRay(x, y, radians, map)
     until i < 1 or i > #map or j < 1 or j > #map[1] or map[math.floor(i)][math.floor(l)] == 1 or map[math.floor(k)][math.floor(j)] == 1
 
     return { xDirection * rayDistance, yDirection * rayDistance }
-end
-
-function utils.emanateRays(x, y, upperRadians, lowerRadians, numLevels, map)
-    if numLevels == 0 then
-        return {}
-    end
-    local centerRadians = (upperRadians + lowerRadians) * .5
-    local rays = { utils.castRay(x, y, centerRadians, map) }
-    for index, value in ipairs(utils.emanateRays(x, y, centerRadians, upperRadians, numLevels - 1, map)) do
-        table.insert(rays, value)
-    end
-    for index, value in ipairs(utils.emanateRays(x, y, centerRadians, lowerRadians, numLevels - 1, map)) do
-        table.insert(rays, value)
-    end
-    return rays
 end
 
 return utils
